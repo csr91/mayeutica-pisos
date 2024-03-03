ee3ef4ceb165aca08317d5b3017ec10e66892d8a
Cesar <cesar.mendoza.77@gmail.com>
2024-03-03 16:00:42 -0300

    pisos

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import subprocess
+import shutil
 
 app = Flask(__name__)
 CORS(app)  # Esto permite solicitudes de otros dominios (CORS)
@@ -12,7 +13,13 @@ def hola_mundo():
 @app.route('/ejecutar_query')
 def ejecutar_query():
     try:
-        # Ejecuta el archivo nd.js utilizando Node.js
+        # Verificar si Node.js está instalado
+        if shutil.which('node') is None:
+            # Node.js no está instalado, intenta instalarlo
+            print("Node.js no está instalado. Instalando...")
+            subprocess.run(['sudo', 'apt', 'install', 'nodejs', '-y'])
+        
+        # Ejecutar el archivo nd.js utilizando Node.js
         resultado = subprocess.check_output(['node', 'nd.js'])
         return jsonify({'resultado': resultado.decode('utf-8')})
     except subprocess.CalledProcessError as e:
